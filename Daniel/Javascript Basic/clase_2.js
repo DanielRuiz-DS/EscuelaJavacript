6f3cc45757c2cc65f4ecbd72948cb3a1a43ba10e
DanielRuiz-DS <danielruizpersonal29@gmail.com>
2022-03-08 22:31:51 -0500

    Liberacion de clase 2

---
+++ Daniel/Javascript Basic/clase_2.js
@@ -0,0 +1,46 @@
+// //FUNCIONES
+// //DECLARATIVA
+// function suma(a,b){
+//     console.log(a + b)
+// }
+// suma(1,5);
+
+
+
+// suma(1,2);
+
+// ///EXPRESIVA
+// restar(5,1);
+// let restar = function(a,b){
+//     console.log(a -b)
+// }
+
+// let hola = "asdasdas";
+//for (let i = 0; i < 9; i++) {
+//     str = str + i;
+// }
+
+
+
+
+function students(){
+    for(let i = users; i <= users.length; i++){
+        console.log("HOLA " + i);
+    };
+};
+
+//students(users);
+
+// function saludar(nombre){
+//     console.log("Hola "+ nombre);
+// }
+
+// saludar("Daniel");
+let users = ["Daniel","Jose","Oscar"];
+///     0        hasta        3 de 1 en 1 
+for(let i = 0; i < users.length; i++){
+    console.log("HOLA " + users[i]);
+};
+
+
+
